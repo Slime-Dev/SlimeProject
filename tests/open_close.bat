6698ca0508c62ada7a9a29cf0157852a3f6e55b1
Alex Mollard <alex.mollard@bigant.com>
2024-05-31 14:19:01 +1000

    Flipping slashes

--- tests/open_close.bat
+++ tests/open_close.bat
@@ -1,34 +1,27 @@
 :: The executable dir is passed as an argument
 :: this test will open the executable and let it run for 5 seconds before closing it
 
-echo off
+@ECHO OFF
+SETLOCAL
 
-:: Check if the executable is passed as an argument
+:: Convert forward slashes to backslashes for compatibility
+SET EXECUTABLE_PATH=%~1
+SET EXECUTABLE_PATH=%EXECUTABLE_PATH:/=\%
 
-IF "%1"=="" (
-    ECHO "Please pass the executable as an argument"
+:: Check if the executable exists
+IF EXIST "%EXECUTABLE_PATH%" (
+    ECHO Starting executable: %EXECUTABLE_PATH%
+    START "" "%EXECUTABLE_PATH%"
+    
+    :: Wait for 5 seconds
+    PING -n 6 > NUL
+    
+    :: Attempt to close the executable
+    TASKKILL /IM "%EXECUTABLE_PATH%" /F
+) ELSE (
+    ECHO Executable not found: %EXECUTABLE_PATH%
     EXIT /B 1
 )
 
-ECHO "Opening and closing the executable"
-
-:: Output the executable path
-ECHO "Executable path: %1"
-
-:: Open the executable
-START "" "%1"
-
-:: Wait for 5 seconds
-PING -n 6
-
-:: Close the executable
-TASKKILL /IM "%1" /F
-
-:: if something goes wrong, exit with error code
-IF ERRORLEVEL 1 (
-    ECHO "Something went wrong"
-    EXIT /B 1
-)
-
-:: Exit
+ENDLOCAL
 EXIT /B 0
