f468996589e4272c2c3d684b8033ad43b511b995
Alex Mollard <alex.mollard@mail.com>
2024-05-31 19:30:25 +1000

    linux test update

--- tests/open_close.sh
+++ tests/open_close.sh
@@ -1,5 +1,6 @@
 # The executable dir is passed as an argument
 # this test will open the executable and let it run for 5 seconds before closing it
+# linux only
 
 # Check if the executable is passed as an argument
 if [ $# -ne 1 ]; then
@@ -9,20 +10,19 @@ fi
 
 # Check if the executable exists
 if [ ! -f $1 ]; then
-    echo "Executable $1 does not exist"
+    echo "Executable not found"
     exit 1
 fi
 
 # Open the executable
 $1 &
 pid=$!
-echo "Executable $1 is running with PID $pid"
 
 # Wait for 5 seconds
 sleep 5
 
 # Close the executable
-echo "Closing executable $1"
-kill -9 $pid
-exit 0
+kill $pid
+wait $pid
 
+exit 0
