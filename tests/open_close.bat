c57f2d5f3cb59e599b5e912c3e5d27d3b5fd1751
Alex Mollard <alex.mollard@bigant.com>
2024-05-31 17:58:58 +1000

    Changed the timeout to use ping instead

--- tests/open_close.bat
+++ tests/open_close.bat
@@ -2,6 +2,7 @@
 :: this test will open the executable and let it run for 5 seconds before closing it
 :: Must not use input redirection or output redirection as it will not work in windows
 
+ECHO OFF
 SET EXECUTABLE_PATH=%~1
 IF "%EXECUTABLE_PATH%"=="" (
     ECHO "Please provide the executable path"
@@ -10,8 +11,8 @@ IF "%EXECUTABLE_PATH%"=="" (
 
 WMIC PROCESS CALL CREATE "%EXECUTABLE_PATH%"
 
-:: Wait for 5 seconds
-TIMEOUT /T 5
+:: Wait for 5 seconds (Cant use input redirection as it will not work in windows)
+ping 127.0.0.1 -n 6 > nul
 
 :: Use WMIC to kill the process
 WMIC PROCESS WHERE "CommandLine LIKE '%%%EXECUTABLE_PATH%%%'" DELETE
