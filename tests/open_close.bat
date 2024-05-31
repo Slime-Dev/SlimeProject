fc30d3a812aad9f6555ce1f5463d50d927022977
Alex Mollard <alex.mollard@bigant.com>
2024-05-31 13:58:06 +1000

    Disabled linux test and updated the windows one

--- tests/open_close.bat
+++ tests/open_close.bat
@@ -1,6 +1,8 @@
 :: The executable dir is passed as an argument
 :: this test will open the executable and let it run for 5 seconds before closing it
 
+echo off
+
 :: Check if the executable is passed as an argument
 
 IF "%1"=="" (
@@ -16,6 +18,12 @@ PING -n 6
 
 :: Close the executable
 TASKKILL /IM "%1" /F
+
+:: if something goes wrong, exit with error code
+IF ERRORLEVEL 1 (
+    ECHO "Something went wrong"
+    EXIT /B 1
+)
 
 :: Exit
 EXIT /B 0
