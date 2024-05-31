e7bc0c284d49e0d00a95b2bbdd718a864ab3c6a4
Alex Mollard <alex.mollard@bigant.com>
2024-05-31 17:14:26 +1000

    another one

--- tests/open_close.bat
+++ tests/open_close.bat
@@ -2,26 +2,11 @@
 :: this test will open the executable and let it run for 5 seconds before closing it
 
 @ECHO OFF
-SETLOCAL
-
-:: Convert forward slashes to backslashes for compatibility
 SET EXECUTABLE_PATH=%~1
 SET EXECUTABLE_PATH=%EXECUTABLE_PATH:/=\%
 
-:: Check if the executable exists
-IF NOT EXIST "%EXECUTABLE_PATH%" (
-    ECHO The executable does not exist
-    EXIT /B 1
-)
-
-:: Open the executable
+ECHO Opening executable at %EXECUTABLE_PATH%
 START "" "%EXECUTABLE_PATH%"
-:: Wait for 5 seconds
-TIMEOUT /T 5 /NOBREAK
-:: Close the executable
-TASKKILL /F /IM "%EXECUTABLE_PATH%" /T
-
-ENDLOCAL
-
-:: Exit with the error code of the executable
-EXIT /B %ERRORLEVEL%
+TIMEOUT /T 5
+TASKKILL /IM %EXECUTABLE_PATH% /F
+ECHO Closed executable at %EXECUTABLE_PATH%
