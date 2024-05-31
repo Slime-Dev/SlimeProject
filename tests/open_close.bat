1378465c0e152281ebdf15bd7a2d69f715cb6608
Alex Mollard <alex.mollard@bigant.com>
2024-05-31 17:35:53 +1000

    Updated script

--- tests/open_close.bat
+++ tests/open_close.bat
@@ -1,11 +1,28 @@
 :: The executable dir is passed as an argument
 :: this test will open the executable and let it run for 5 seconds before closing it
+:: Must not use input redirection or output redirection as it will not work in windows
 
 @ECHO OFF
 SET EXECUTABLE_PATH=%~1
 
-ECHO Opening executable at %EXECUTABLE_PATH%
-START "" "%EXECUTABLE_PATH%"
-TIMEOUT /T 5
-TASKKILL /IM %EXECUTABLE_PATH% /F
-ECHO Closed executable at %EXECUTABLE_PATH%
+:: Get just the executable name eg. "SlimeOdyssey.exe"
+SET EXECUTABLE_NAME=%~nx1
+
+IF "%EXECUTABLE_PATH%"=="" (
+    ECHO "Please provide the executable path"
+    EXIT /B 1
+)
+
+START /wait "" "%EXECUTABLE_PATH%"
+IF ERRORLEVEL 1 (
+    ECHO "Failed to start the executable"
+    EXIT /B 1
+)
+
+:: Wait for 5 seconds
+PING -n 6
+
+:: Close the executable
+TASKKILL /IM "%EXECUTABLE_NAME%" /F
+
+EXIT /B 0
