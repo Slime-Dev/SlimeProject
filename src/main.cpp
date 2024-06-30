0a0f9547aea3f3271e9a2a395b1ccd8d58b22edc
Alex Mollard <alex.mollard@mail.com>
2024-06-30 17:39:23 +1000

    Auto setting shader dir

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,26 @@
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/stdout_color_sinks-inl.h"
 
-#define BUILD_DIRECTORY "/home/alexm/Source/SlimeOdyssey/Shaders"
+#include <filesystem>
+#include <string>
+namespace fs = std::filesystem; // For convenience
 
-const int MAX_FRAMES_IN_FLIGHT = 2;
+// Function to get the build directory relative to the source file
+std::string getBuildDirectory() {
+	// Get the absolute path of the current source file
+	auto srcPath = fs::absolute(__FILE__);
 
+	return fs::path(srcPath).parent_path().parent_path().string();
+}
 int main()
 {
 	spdlog::set_level(spdlog::level::trace);
 	spdlog::stdout_color_mt("console");
+	std::string buildDir = getBuildDirectory();
+
+	spdlog::info("Build directory: {}", buildDir);
+
+	const std::string shadersDir = buildDir + "/Shaders";
 
 	SlimeEngine::Init init;
 	SlimeEngine::RenderData data;
@@ -23,7 +35,7 @@ int main()
 		return -1;
 	if (SlimeEngine::CreateSwapchain(init, data) != 0)
 		return -1;
-	if (SlimeEngine::CreateGraphicsPipeline(init, data, BUILD_DIRECTORY) != 0)
+	if (SlimeEngine::CreateGraphicsPipeline(init, data, shadersDir.c_str()) != 0)
 		return -1;
 	if (SlimeEngine::RecordCommandBuffers(init, data) != 0)
 		return -1;
