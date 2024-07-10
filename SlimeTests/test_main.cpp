f88141adeb8d2ef681d91d56a35bf10bd2276792
Connor Young <c_young97@live.com>
2024-07-10 19:35:38 +1000

    Init Testing
    - Updating the Engine initialization testing
    - Git actions will now trigger on push to any branch, but only respond to pull requests on the main branch
    - Improved naming of tests

---
+++ SlimeTests/test_main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "Engine.h"
+#include "spdlog/spdlog.h"
+#include "ShaderManager.h"
+#include "PipelineGenerator.h"
+#include "ResourcePathManager.h"
+#include "ModelManager.h"
+#include <vector>
+
+void testEngineInitialization() {
+	SlimeWindow::WindowProps windowProps{ .title = "Slime Odyssey", .width = 1920, .height = 1080, .resizable = true, .decorated = true, .fullscreen = false };
+
+	SlimeWindow window(windowProps);
+
+	Engine engine(&window);
+
+    assert(engine.CreateEngine() == 0);
+}
+
+int main() {
+    try {
+        // Run tests
+        testEngineInitialization();
+
+        std::cout << "All tests passed!" << std::endl;
+        return 0;
+    } catch (const std::exception& e) {
+        std::cerr << "Test failed with exception: " << e.what() << std::endl;
+        return 1;
+    } catch (...) {
+        std::cerr << "Unknown exception occurred during testing." << std::endl;
+        return 2;
+    }
+}
